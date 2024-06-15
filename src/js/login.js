094320c5cd1833452b8318d4ee8b0f00d870e862
cavanagh <bartholomewFlash@qq.com>
2024-06-15 12:02:48 +0800

    Updates

--- src/js/login.js
+++
@@ -1,3 +0,0 @@
-import '../scss/login.scss'
-import '../fonts/iconfont.css'
-import { $, $A } from './utils'
--- src/js/utils.js
+++
@@ -1,3 +0,0 @@
-export const $ = (el, parent = document) => parent.querySelector(el)
-export const $all = (el, parent = document) => [...parent.querySelectorAll(el)]
-export const $A = (el, parent = document) => [...parent.querySelectorAll(el)]
